058002ca953ec2b1c05ec7d7c9bfadaef42ea4b1
Stage IT <stageit@akili-consulting.com>
2024-05-20 10:32:14 +0200

    page accueil

---
+++ front/accueil.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon Blog</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-white rounded">
+    <div class=" bg-black rounded shadow-xl h-20">
+        <div class="flex items-center space-x-3">
+            <img src="assets/dDDunia-removebg-preview.png" alt="Mon logo" class="border-2 border-red-400  w-24 h-20 ">
+            <div class="text-white">DUNIA</div>
+        </div>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</body>
+</html>
