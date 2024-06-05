87e10e06f58521e303071fa98d4bfc6a0eabf67b
Stage IT <stageit@akili-consulting.com>
2024-06-05 08:57:54 +0200

    service part

--- front/accueil.php
+++ front/accueil.php
@@ -6,39 +6,44 @@
     <link rel="stylesheet" href="index.css">
     <title>Mon Blog</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="assets/tailwind.css" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/cd48048bdc.js" crossorigin="anonymous"></script>
 </head>
 <body class="bg-white rounded ">
-    <div class="flex bg-black rounded shadow-xl h-18 px-6 justify-between fixed z-50 left-0 right-0 top-0"> 
-        <div class="flex items-center">
-            <img src="assets/dDDunia-removebg-preview.png" alt="Mon logo" class="w-24 h-16 ">
-            <div class="text-white font-bold text-4xl">DUNIA</div>
-        </div>
-        <div class="flex text-white font-bold text-sm items-center gap-5 cursor-pointer">
-            <div>HOME</div>
-            <div>SERVICES</div>
-            <div>PROJETS</div>
-            <div>REVIEWS</div>
-            <div>BLOG</div>
-            <div>TEAM</div>
-            <div>CONTACT US</div>
-        </div>
-    </div>
-
-    <div class=" bg-cover bg-center h-screen flex flex-col text-white items-center pt-20 px-56 text-center gap-y-6 " style="background-image: url('assets/hector-j-rivas-1FxMET2U5dU-unsplash.jpg');">
-        <div class="text-5xl font-semibold pb-4">Lorem Ipsum</div>
-        <div class="leading-7">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-             Quam deserunt, officiis praesentium obcaecati quidem quasi
-              architecto impedit est, asperiores facere voluptates unde voluptatibus
-               ipsam error saepe accusamus amet natus dolor.
-        </div>
-        <button type="button" class="bg-blue-700 p-2 rounded-lg w-40">Know More</button>
-        <div class="h-14 w-8 bg-cover border-2 border-white rounded-full flex items-center justify-center">
-            <div class="h-1 w-1 bg-cover bg-white rounded-full animate-bounce">
+    <div class="pb-20">
+        <div class="flex bg-black rounded shadow-xl h-18 px-16 py-3 justify-between fixed z-50 left-0 right-0 top-0 "> 
+            <div class="flex items-center gap-2">
+                <i class="fa-solid fa-infinity  text-5xl" style="color: #e84a4e;"></i>
+                <div class="text-white font-bold text-4xl">DUNIA</div>
+            </div>
+            <div class="flex text-white font-bold text-sm items-center gap-5 cursor-pointer">
+                <div>HOME</div>
+                <div>SERVICES</div>
+                <div>PROJETS</div>
+                <div>REVIEWS</div>
+                <div>BLOG</div>
+                <div>TEAM</div>
+                <div>CONTACT US</div>
             </div>
         </div>
 
-        
-       
+        <div class=" bg-cover bg-center h-screen flex flex-col text-white items-center pt-36 px-56 text-center gap-y-6 " style="background-image: url('assets/hector-j-rivas-1FxMET2U5dU-unsplash.jpg');">
+            <div class="text-5xl font-semibold pb-4">Lorem Ipsum</div>
+            <div class="leading-7">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quam deserunt, officiis praesentium obcaecati quidem quasi
+                architecto impedit est, asperiores facere voluptates unde voluptatibus
+                ipsam error saepe accusamus amet natus dolor.
+            </div>
+            <button type="button" class="bg-blue-700 p-2 rounded-lg w-40">Know More</button>
+            <div class="h-14 w-8 bg-cover border-2 border-white rounded-full flex items-center justify-center">
+                <div class="h-1 w-1 bg-cover bg-white rounded-full animate-bounce">
+                </div>
+            </div>
+        </div>
     </div>
+    
+    <?php include 'services.php';?>
 </body>
 </html>
---
+++ front/services.php
@@ -0,0 +1,97 @@
+<div class="flex gap-4 px-10 items-center mb-20">
+        <?php 
+        for($i=1;$i<=4;$i++){
+            echo"
+            <div class=' flex flex-col items-center text-center space-y-6'>
+                <i class='fa-regular fa-object-group text-6xl pb-4 drop-shadow-2xl' style='color: #b89e89;'></i>
+                <div class='text-blue-900 font-bold text-2xl'>App Design</div>
+                <div class='text-[#516C90] leading-7 text-xl'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Optio veniam soluta similique totam. 
+                </div>
+            </div>";
+        }
+    ?>
+</div>
+    <div class="bg-red-100 flex flex-row px-10 pt-10 gap-10 pb-20">
+        <div class=" flex flex-col gap-4 w-2/3">
+            <div class="font-semibold text-2xl quoted-text">
+                "If you want to buil somethine big, you have to start with a small step!" 
+            </div>
+            <div class="text-gray-400">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
+                 cupiditate consequatur tempora minus ea recusandae praesentium, labore
+                quidem. Expedita ratione optio itaque obcaecati animi architecto porro 
+                culpa exercitationem officia quas!
+            </div>
+            <div class="bg-white drop-shadow-xl flex justify-between rounded items-center p-4">
+                <div class="flex gap-4 items-center">
+                    <i class='fa-regular fa-object-group text-4xl pb-4 drop-shadow-2xl' style='color: #b89e89;'></i>
+                    <div>
+                        <div class="text-3xl font-bold ">1458</div>
+                        <div class="text-gray-400">Project completed</div>
+                    </div>
+                </div>
+                <div class="flex gap-4 items-center">
+                    <i class="fa-solid fa-mug-saucer text-4xl pb-4 drop-shadow-2xl" style="color: #b89e89;""></i>
+                    <div>
+                        <div class="text-3xl font-bold">1247</div>
+                        <div class="text-gray-400">Cups of coffee</div>
+                    </div>
+                </div>
+                <div class="flex gap-4 items-center">
+                    <i class="fa-solid fa-earth-europe text-4xl pb-4 drop-shadow-2xl" style="color: #b89e89;"></i>
+                    <div class="">
+                        <div class="text-3xl font-bold">1763</div>
+                        <div class="text-gray-400">Worldwide clients</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bg-cover bg-center w-1/3 rounded" style="background-image: url('assets/vladislav-todorov-joINR2EKnvY-unsplash.jpg');">
+            <div class="flex flex-col gap-4 px-12 bg-red-100 opacity-50 h-64 m-3 justify-center rounded">
+                <div class="text-2xl font-bold">SINCE</div>
+                <div class="text-5xl font-bold">1998</div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-blue-700 px-16 py-10 flex flex-col items-center justify-center gap-5">
+        <div class="text-white font-bold text-3xl">Find out more about what we do</div>
+        <div class="text-center text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci consectetur velit et molestiae eaque. 
+            Recusandae error dignissimos voluptate quam repellat veritatis est 
+            cupiditate libero, quae, quisquam hic, laboriosam nulla voluptates!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci consectetur velit et molestiae eaque. 
+            Recusandae error dignissimos voluptate quam repellat veritatis est 
+            cupiditate libero, quae, quisquam hic, laboriosam nulla voluptates!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci consectetur velit et molestiae eaque. 
+            Recusandae error dignissimos voluptate quam repellat veritatis est 
+            cupiditate libero, quae, quisquam hic, laboriosam nulla voluptates!
+        </div>
+        <button class="bg-black text-white p-3 rounded-lg font-semibold">
+            Learn Methods
+        </button>
+    </div>
+    <div class="flex">
+        <?php
+        for($i=1;$i<=3;$i++){
+            echo"
+            <div class='p-10 flex flex-col items-start gap-5'>
+                <div>Web Development For Clients And Market</div>
+                <div>
+                    Recusandae error dignissimos voluptate quam repellat veritatis est 
+                    cupiditate libero, quae, quisquam hic, laboriosam nulla voluptates!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Adipisci consectetur velit et molestiae eaque. 
+                    Recusandae error dignissimos voluptate quam repellat veritatis est 
+                    cupiditate libero, quae, quisquam hic, laboriosam nulla voluptates!
+                </div>
+                <button>Contact</button>
+            </div>
+            ";
+        }
+        ?>
+    </div>
